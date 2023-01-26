862a734cf8b8bc69bad9b229f21de855efae7579
jaehyunjang <hyuni2020@gmail.com>
2023-01-27 00:13:26 +0900

    23-01-26: Framer Motion - Clean How to use Hover & drag

--- src/App.tsx
+++ src/App.tsx
@@ -12,58 +12,27 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: grid;
-  /* children 2x2 */
-  grid-template-columns: repeat(2, 1fr);
+`
 
-  /* center children */
-  place-items: center;
-`
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  border-radius: 50%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`
 const boxVars = {
-  start: {
-    opacity: 0,
-    scale: 0,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      delayChildren: 0.3,
-      staggerChildren: 0.2,
-    },
-  },
-}
-
-const circleVars = {
-  start: {
-    y: 20,
-    opacity: 0.1,
-  },
-  end: {
-    y: 0,
-    opacity: 1,
-  },
+  hover: { rotate: 90, scale: 1.3 },
+  click: { borderRadius: '100px', scale: 1, rotate: -90 },
+  drag: { backgroundColor: 'rgb(46, 204, 113)', transition: { duration: 2 } },
 }
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVars} initial="start" animate="end">
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-      </Box>
+      <Box
+        drag
+        variants={boxVars}
+        whileHover="hover"
+        whileTap="click"
+        whileDrag="drag"
+      ></Box>
     </Wrapper>
   )
 }
